13c4219d52a24691d52018a0a2ad0c2071ab8680
Arpit <111arpit1@gmail.com>
2025-07-14 23:23:56 +0530

    removed creds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import config from "./config/config"
+import {Header,Footer} from "./components/index"
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
 function App() {
-  console.log("env variables",config)
+  const[loading,setLoading] = useState(true);
+  const dispath = useDispatch();
+  useEffect(()=>{
+    const user = authService.getCurrentUser();
+    console.log("user",user)
+  },[])
+
   return (
     <>
-    
+    <Header/>
     CodeSmashers Blog
+    <Footer/>
     </>
   );
 }
